fbeac086ebbe01ddd30c01bd15ab16e05f5ecfd5
GPTalpa <120nik01@mail.ru>
2026-01-27 06:04:15 +0200

    fixes

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 // src/lib/constants.ts
 
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
+  process.env.NEXT_PUBLIC_API_URL || "http://147.45.108.163:4000";
 
 export const ROLES = {
   USER: "user",
